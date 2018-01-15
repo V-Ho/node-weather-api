a9c3fe52da04ac9f396bf7f1727230e780f63a8e
Vanessa Ho <vanessa@info.nl>
2018-01-15 11:24:41 +0100

    add error handling for server error and no results found

--- app.js
+++ app.js
@@ -13,20 +13,29 @@ const argv = yargs
   })
   .help()
   .alias('help', 'h')   //setting up help alias with 2 args $ node app.js --help
-  .argv                 //stores everything above in const argv
+  .argv;                 //stores everything above in const argv
 
 console.log(argv)     //will log everything parsed by yargs
 
-var encodedAddress = encodeURIComponent(argv.address)
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCr9jxyudzQS03YhkjELuLSB0kKfREomi8&address=${encodedAddress}`,
+  url: `https://map.googleapis.com/maps/api/geocode/json?key=AIzaSyCr9jxyudzQS03YhkjELuLSB0kKfREomi8&address=${encodedAddress}`,
   json: true  //data recieved as jsons
 }, (error, response, body) => {
-  console.log(JSON.stringify(error, undefined, 2)) //shows JSON error in detail
-  // console.log(JSON.stringify(body, undefined, 2)) //shows JSON body in detail
-  console.log(`Address: ${body.results[0].formatted_address}`)
-  console.log(`Lat: ${body.results[0].geometry.location.lat} & Lng: ${body.results[0].geometry.location.lng}`)
+  if (body.status === 'OK') {
+    // console.log(JSON.stringify(error, undefined, 2)) //shows JSON error in detail
+    // console.log(JSON.stringify(body, undefined, 2)) //shows JSON body in detail
+    console.log(`Address: ${body.results[0].formatted_address}`)
+    console.log(`Lat: ${body.results[0].geometry.location.lat} &
+      Lng: ${body.results[0].geometry.location.lng}`)
+  }
+  else if (body.status === "ZERO_RESULTS") {
+    console.log('Unable to find that address');
+  }
+  else {
+    console.log('Unable to connect to server')
+  }
 })
 
 
@@ -43,7 +52,7 @@ Request: uri object includes protocol (https), port, hostname
 StatusCode: HTTP errors ex 200
 
 Run code with:
-$node app.js -a '488 Heatherhill Pl Waterloo
+$node app.js -a '488 Heatherhill Pl Waterloo'
 
 Here we input our desired address in the command line.
 App.js fetches formatted address, lat and lng.  The address is then passed to an encodedURI
