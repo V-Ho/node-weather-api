8429fd7d6c99a6e684c242e0290bdebb2d6b7c67
Vanessa Ho <vanessa@info.nl>
2018-01-17 16:11:13 +0100

    refactor code so fetch is in separate geocode file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const request = require('request')
 const yargs = require('yargs') //used to build interactive command line tools
 
+const geocode = require('./geocode/geocode.js')
+
 //OPTIONS CONFIGURATION FOR USER INPUT IN COMMAND LINE
 const argv = yargs
   .options({
@@ -17,39 +19,9 @@ const argv = yargs
 
 console.log(argv)     //will log everything parsed by yargs
 
-var encodedAddress = encodeURIComponent(argv.address);
+geocode.geocodeAddress(argv.address) //takes in address from argv command-line
 
-request({
-  url: `https://map.googleapis.com/maps/api/geocode/json?key=AIzaSyCr9jxyudzQS03YhkjELuLSB0kKfREomi8&address=${encodedAddress}`,
-  json: true  //data recieved as jsons
-}, (error, response, body) => {
-  if (body.status === 'OK') {
-    // console.log(JSON.stringify(error, undefined, 2)) //shows JSON error in detail
-    // console.log(JSON.stringify(body, undefined, 2)) //shows JSON body in detail
-    console.log(`Address: ${body.results[0].formatted_address}`)
-    console.log(`Lat: ${body.results[0].geometry.location.lat} &
-      Lng: ${body.results[0].geometry.location.lng}`)
-  }
-  else if (body.status === "ZERO_RESULTS") {
-    console.log('Unable to find that address');
-  }
-  else {
-    console.log('Unable to connect to server')
-  }
-})
-
-
-//request takes 2 arguments: options object (ex maps data) & callback function
-//options object: unique url property & json property
-//callback: after url reached, then we call (error, response, body)
-/* Request callback includes 3 arguments: once the url is recieved, the callback response includes:
-Error: errors made during HTTP request - ex address incorrect, machine doesn't have access
-Response: entire response object, including uri, headers, method, body
-Body: the body of json data returned by Google API
-
-Headers: sent from Google API server to Node server, accepting application/JSON
-Request: uri object includes protocol (https), port, hostname
-StatusCode: HTTP errors ex 200
+/*
 
 Run code with:
 $node app.js -a '488 Heatherhill Pl Waterloo'
---
+++ geocode/geocode.js
@@ -0,0 +1,50 @@
+const request = require('request')
+
+
+exports.geocodeAddress = (address) => {
+  var encodedAddress = encodeURIComponent(address);
+
+  request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCr9jxyudzQS03YhkjELuLSB0kKfREomi8&address=${encodedAddress}`,
+    json: true  //data recieved as jsons
+  }, (error, response, body) => {
+    if (body.status === 'OK') {
+      // console.log(JSON.stringify(error, undefined, 2)) //shows JSON error in detail
+      // console.log(JSON.stringify(body, undefined, 2)) //shows JSON body in detail
+      console.log(`Address: ${body.results[0].formatted_address}`)
+      console.log(`Lat: ${body.results[0].geometry.location.lat} &
+        Lng: ${body.results[0].geometry.location.lng}`)
+    }
+    else if (body.status === "ZERO_RESULTS") {
+      console.log('Unable to find that address');
+    }
+    else {
+      console.log('Unable to connect to server')
+    }
+  })
+
+}
+
+
+//request takes 2 arguments: options object (ex maps data) & callback function
+//options object: unique url property & json property
+//callback: after url reached, then we call (error, response, body)
+/* Request callback includes 3 arguments: once the url is recieved, the callback response includes:
+Error: errors made during HTTP request - ex address incorrect, machine doesn't have access
+Response: entire response object, including uri, headers, method, body
+Body: the body of json data returned by Google API
+
+Headers: sent from Google API server to Node server, accepting application/JSON
+Request: uri object includes protocol (https), port, hostname
+StatusCode: HTTP errors ex 200
+
+Run code with:
+$node app.js -a '488 Heatherhill Pl Waterloo'
+
+Here we input our desired address in the command line.
+App.js fetches formatted address, lat and lng.  The address is then passed to an encodedURI
+component, which is injected into the url.
+
+
+
+*/
